45d132f4349afca4ffe6b75e5fc551ef954ea4fd
jessabc <120062852+jessabc@users.noreply.github.com>
2023-04-11 21:29:51 +1000

    css

--- src/components/Header/BoardLink.jsx
+++ src/components/Header/BoardLink.jsx
@@ -25,7 +25,7 @@ export default function BoardLink({board, setIsMenuModalVisible}) {
    
     return (
         <div 
-            className={`${currentBoardName === board.name ? 'bg-indigo-500 text-gray-50 rounded-tr-full rounded-br-full hover:bg-indigo-500 hover:text-gray-50 hover:rounded-tr-full hover:rounded-br-full py-3 w-full' : 'hover:text-gray-50 hover:bg-indigo-400  hover:rounded-tr-full py-3 my-1 hover:rounded-br-full '} flex items-center gap-2  py-1 cursor-pointer`} 
+            className={`${currentBoardName === board.name ? 'bg-indigo-500 text-gray-50 rounded-tr-full rounded-br-full hover:bg-indigo-500 hover:text-gray-50 hover:rounded-tr-full hover:rounded-br-full py-3 w-full' : 'hover:text-gray-50 hover:bg-indigo-400  hover:rounded-tr-full py-3  hover:rounded-br-full '} flex items-center gap-2  my-1 cursor-pointer`} 
             onClick={handleClick}>
             <img src={iconBoard} alt='' className={`w-4 h-4 ml-5 ${currentBoardName === board.name ? 'ml-0  ': ' '} `}/>
             <p className=' '>{board.name}</p>
--- src/components/Main/NewColumn.jsx
+++ src/components/Main/NewColumn.jsx
@@ -8,7 +8,7 @@ export default function NewColumn() {
 
     return (
         <>
-            <div className='rounded-lg bg-gray-300 mt-20 h-screen'>
+            <div className='rounded-lg bg-gray-300 dark:bg-zinc-700 mt-20 h-screen'>
                 <div className='text-gray-500 hover:text-gray-600 mt-20 mx-10  text-center w-44 font-semibold text-xl '>
                     <button onClick={() => setIsAddNewColumnModalVisible
                     (true)}>
